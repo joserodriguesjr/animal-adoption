258202dd42aa6e7dcaf6328d258c99ec9caf8c15
runiorr <joseluisrjunior@gmail.com>
2024-10-11 16:09:43 -0300

    feat: update status throws EntityNotFoundException if id doenst exist

--- src/main/java/com/example/adoption/application/controller/AnimalController.java
+++ src/main/java/com/example/adoption/application/controller/AnimalController.java
@@ -4,12 +4,13 @@ import com.example.adoption.application.form.UpdateAnimalStatusForm;
 import com.example.adoption.domain.model.Animal;
 import com.example.adoption.domain.service.AnimalService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @AllArgsConstructor
@@ -19,11 +20,17 @@ public class AnimalController {
 
     private AnimalService animalService;
 
-    @PutMapping(path= "/{id}/status", consumes = "application/json", produces = "application/json")
+    @PatchMapping(path= "/{id}/status", consumes = "application/json", produces = "application/json")
     public ResponseEntity<Animal> updateStatus(
             @PathVariable(value = "id") Long id,
             @RequestBody UpdateAnimalStatusForm statusForm) {
         Animal.Status status = statusForm.status();
-        return ResponseEntity.status(HttpStatus.CREATED).body(animalService.updateStatus(id, status));
+
+        try {
+            Animal updatedAnimal = animalService.updateStatus(id, status);
+            return ResponseEntity.ok(updatedAnimal);
+        } catch (EntityNotFoundException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
--- src/main/java/com/example/adoption/domain/service/AnimalService.java
+++ src/main/java/com/example/adoption/domain/service/AnimalService.java
@@ -2,6 +2,7 @@ package com.example.adoption.domain.service;
 
 import com.example.adoption.domain.model.Animal;
 import com.example.adoption.domain.repository.AnimalRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +13,7 @@ public class AnimalService {
     private final AnimalRepository animalRepository;
 
     public Animal updateStatus(Long id, Animal.Status status) {
-        Animal animal = animalRepository.findById(id).orElseThrow();
+        Animal animal = animalRepository.findById(id).orElseThrow(EntityNotFoundException::new);
         animal.setStatus(status);
         return animalRepository.save(animal);
     };
